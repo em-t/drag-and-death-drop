e00ee54264af75739ed7a28a5f86678101c59a69
jade.paukkunen <jademariia.paukkunen@gmail.com>
2018-11-16 08:17:33 +0200

    etusivun keskeneräinen ulkoasu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return <div className="App">
+          <body className="App-header">
+              <div className="grid-container">
+                  <div className="lefttop"><img src={logo} className="logo" alt="logo" /></div>
+                  <div className="leftbottom">WELCOME TO THE RUPAUL'S DRAG AND DROP DEAD RACE!</div>
+                  <div className="middle"></div>
+                  <div className="righttop"><p>GENTLEMEN, START YOUR ENGINES, AND MAY THE BEST WOMAN WIN</p></div>
+                  <div className="rightbottom"></div>
+              </div>
+          </body>
+      </div>;
   }
 }
 
